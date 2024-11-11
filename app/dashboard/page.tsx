f65b67034b72645f7b7f4a86a945abb7941bbf02
UdayPandey01 <pandeyuday113@gmail.com>
2024-11-11 23:23:53 +0530

    googleLogin

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { FC } from "react";
+
+interface PageProps {}
+
+const page : FC<PageProps> = ({}) => {
+    return (
+        <Button>Hello</Button>
+    )
+}
+
+export default page
