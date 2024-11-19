7beed83073456ebb5beccdd7511bb53f91eaf6a8
UdayPandey01 <pandeyuday113@gmail.com>
2024-11-19 11:44:53 +0530

    AddFriend

--- app/api/friends/add/route.ts
+++ app/api/friends/add/route.ts
@@ -1,7 +1,9 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   try {
@@ -33,7 +35,7 @@ export async function POST(req: Request) {
 
     if (idToAdd === session.user.id) {
       return new Response("You cannot add yourself", { status: 400 });
-    }
+    } 
 
     const isAlreadyAdded = (await fetchRedis(
       `sismember`,
@@ -41,16 +43,28 @@ export async function POST(req: Request) {
       session.user.id
     )) as 0 | 1;
 
+    if (isAlreadyAdded) {
+      return new Response("Already added this user", { status: 400 });
+    }
+
     const isAlreadyFriends = (await fetchRedis(
       `sismember`,
       `user:${session.user.id}:friends`,
       idToAdd
     )) as 0 | 1;
 
-    if (isAlreadyAdded) {
-      return new Response("Already added this user", { status: 400 });
+    if (isAlreadyFriends) {
+        return new Response("Already friends with this user", { status: 400 });
+      }
+
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+
+    return new Response('OK', {status : 200})
+  } catch (error) {
+    if(error instanceof z.ZodError){
+        return new Response('Invalid status payload ', {status : 422})
     }
 
-    console.log(data);
-  } catch (error) {}
+    return new Response('Invalid request', {status : 400})
+  }
 }
