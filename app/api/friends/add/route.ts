bc38ffe6fa22893f277fd5aaa90388227bbdc8be
UdayPandey01 <pandeyuday113@gmail.com>
2024-11-24 14:49:05 +0530

    addedAcceptDenyroutes

--- app/api/friends/add/route.ts
+++ app/api/friends/add/route.ts
@@ -10,21 +10,14 @@ export async function POST(req: Request) {
     const body = await req.json();
 
     const { email: emailToAdd } = addFriendValidator.parse(body.email);
-    const RESTResponse = await fetch(
-      `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
-        },
-        cache: "no-store",
-      }
-    );
-
-    const data = (await RESTResponse.json()) as { result: string | null };
-    const idToAdd = data.result;
+    
+    const idToAdd = (await fetchRedis(
+      'get',
+      `user:email:${emailToAdd}`
+    )) as string
 
     if (!idToAdd) {
-      return new Response("This person does not exist", { status: 400 });
+      return new Response('This person does not exist.', { status: 400 })
     }
 
     const session = await getServerSession(authOptions);
